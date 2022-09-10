d7157c247712f1453d9027cd15a5e03ab1d1644c
Jing0001 <feng.jing1@northeastern.edu>
2022-09-10 13:52:23 -0700

    Add About Me Button

--- app/src/main/java/edu/northeastern/numad22fa_jingfeng/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_jingfeng/MainActivity.java
@@ -3,6 +3,10 @@ package edu.northeastern.numad22fa_jingfeng;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(view -> {
+            Log.i("MyApp", "This is message");
+            Toast.makeText(getApplicationContext(), "Jing Feng, feng.jing1@northeastern.edu", Toast.LENGTH_SHORT).show();
+        });
     }
 }
